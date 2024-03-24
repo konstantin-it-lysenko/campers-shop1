4da624fd7967576270c86d6bc8d9973552c76103
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-24 23:51:56 +0200

    remove

--- src/redux/contacts/contactsSelectors.js
+++
@@ -1,3 +0,0 @@
-export const selectContacts = ({ contacts }) => contacts.items;
-export const selectLoading = ({ contacts }) => contacts.isLoading;
-export const selectError = ({ contacts }) => contacts.error;
