d6538b41dc47dfa5db0323fbba1ba22a37750259
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-24 23:55:48 +0200

    add global theme styles

---
+++ src/constants/theme.jsx
@@ -0,0 +1,18 @@
+export const theme = Object.freeze({
+  colors: {
+    lightgray: '#F2F4F7',
+    active: '#D84343',
+    fontPrimary: '#101828',
+    fontSecondary: '#475467',
+    red: '#E44848',
+    yellow: '#FFC531',
+    white: '#ffffff',
+  },
+  border: {
+    onePx: `1px solid #10182833`,
+    twoPx: `2px solid #101828`,
+  },
+  transition: {
+    cubic: `.25s cubic-bezier(0.4, 0, 0.2, 1)`,
+  },
+});
