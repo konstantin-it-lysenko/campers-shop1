c7deb6c27fa32a144b47acf7c5a3ba32d2edc9dc
Konstantin Lysenko <konstantin.it.lysenko@gmail.com>
2024-03-24 23:54:06 +0200

    add store and browserrouter

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './redux/store';
-import App from 'components/App/App';
-import './reset.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./components/App/App";
+import "./reset.css";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter basename="/campers-shop">
+        <App />
+      </BrowserRouter>
     </Provider>
-  </React.StrictMode >
+  </React.StrictMode>
 );
